539ff74ce2507689b7eb1e1fad855fe3d55fdfca
arshil <arshilsingh711@gmail.com>
2024-06-30 14:50:05 +0530

    Added requirements and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "GenAI Interview Question creation",
+    version= '0.0.1',
+    author= 'Arshil Singh Bhatia',
+    author_email='arshilsingh711@gmail.com',
+    packages= find_packages(),
+    install_requires = [] 
+)
